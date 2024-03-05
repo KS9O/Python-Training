a9600e498846514c845f9fbc8c6c171b8567ceac
KS9O <132106064+KS9O@users.noreply.github.com>
2024-03-04 19:03:07 -0500

    Create pylesson1.py

---
+++ pylesson1.py
@@ -0,0 +1,27 @@
+# print is to be involved w/ parentheses in Python 3
+print("Hello, this will print.")
+# Python uses indentation for blocks, instead of curly braces. Standard indentation requires standard python code to use four spaces.
+x = 1
+if x == 1: # we use the colon to indicate a new block of code, such as a loop, function or conditional statement
+# indented four spaces, this is [tab] 
+    print("x is 1.")
+# Python supports two types of numbers - integers (whole numbers) (int) and floating point numbers(decimals)
+myint = 90
+print(myint)
+# To define a floating point number, you may use one of the following notations
+myfloat = 90.0
+print(myfloat)
+# Or this notation
+myfloat = float(90)
+print(myfloat)
+# Strings, defined either by a single quote(') or double quote(")
+mystring = 'hello'
+print(mystring)
+mystring = "Hello"
+print(mystring)
+# The difference between the two is hat using double quotes makes it easy to include apostrophes ( whereas these would terminate the string if using single quotes)
+mystring = "Don't worry about apostrophes"
+print(mystring)
+mystring = 'Don't worry about apostrophes'      # see the curly brackets
+
+
